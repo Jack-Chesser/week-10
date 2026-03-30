bd1d69c8a58dc9e60b59a91eb3e5eaab0e7d2b5b
Jack-Chesser <chessjac000@gmail.com>
2026-03-29 21:29:39 -0500

    creaded the fetch button and got Gifs to apear

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
 console.log("script.js loaded");
+
+const apiKey = "spc5YKzm7OOTps2UbwQM7IH8pj53RZto";
+
+let endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=funny&limit=12`;
+
+async function fetchGifs() {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+
+  // Step 7 requirement: store original image URLs in an array called images
+  const images = data.data.map(gif => gif.images.original.url);
+
+  console.log(images); // preview in console
+
+  return images;
+}
+
+const gifContainer = document.querySelector("#gif-container");
+const button = document.querySelector("#fetch-gif-btn");
+
+button.addEventListener("click", async () => {
+  const images = await fetchGifs(); // call your Step 7 function
+
+  gifContainer.innerHTML = ""; // clear old gifs
+
+  for (let img of images) {
+    gifContainer.innerHTML += `
+      <img src="${img}" class="col-3 mb-3">
+    `;
+  }
+});
